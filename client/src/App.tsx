d75ece6e4d53f016c7d20aa7f9686f230329bc14
admin <52886942+webdirection-dev@users.noreply.github.com>
2022-09-17 17:11:23 +0300

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,15 +4,12 @@ import Header from "./layout/header/Header"
 import Main from "./layout/main/Main"
 import Footer from "./layout/footer/Footer"
 
-const  App = () => {
-
-    return (
-        <div className="App">
-            <Header />
-            <Main />
-            <Footer />
-        </div>
-    )
-}
+const  App = () => (
+    <div className="App">
+        <Header />
+        <Main />
+        <Footer />
+    </div>
+)
 
 export default App
